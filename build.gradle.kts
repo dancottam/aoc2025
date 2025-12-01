d59e852bb7f4f3fb00c01389f61afa2b86c11087
Dan Cottam <dan.cottam@landregistry.gov.uk>
2025-12-01 12:20:56 +0000

    Initial set up

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,18 @@ plugins {
     kotlin("jvm") version "2.2.21"
 }
 
-sourceSets {
-    main {
-        kotlin.srcDir("src")
-    }
+dependencies {
+    val junitVersion = "6.0.1"
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.assertj:assertj-core:3.24.2")
 }
 
 tasks {
     wrapper {
         gradleVersion = "9.2.1"
+    }
+    test {
+        useJUnitPlatform()
     }
 }
